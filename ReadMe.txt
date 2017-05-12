1b62450fe4c80ef22d16699d94aad62719961583
papago <kuo.sunnyfruit@gmail.com>
2017-05-12 23:55:39 +0800

    Env init

--- ReadMe.txt
+++ ReadMe.txt
@@ -3,3 +3,11 @@ npm init
 npm i -D webpack@2.2.0
 
 npm i -D html-webpack-plugin
+
+npm i -D css-loader
+
+npm i -D style-loader
+
+npm i -D webpack-dev-server@2.2.0
+
+npm run dev
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
 
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: 'dist',
+        path: path.resolve(__dirname, "dist"),
         filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
     },
     plugins: [
         new HtmlWebpackPlugin({
